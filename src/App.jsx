ba42db398c45b538fa716293945295e0827077d9
Fexxonn <torrenegradavid@gmail.com>
2025-01-29 12:30:37 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import "./App.css";
+
+export default function ToDoList() {
+  const [texto, setTexto] = useState("");
+  const [tareas, setTareas] = useState(
+    JSON.parse(localStorage.getItem("tareas")) || []
+  );
+  const ActualizarTexto = (e) => setTexto(e.target.value);
+
+  const AgregarTarea = () => {
+    if (texto.trim()) {
+      const nuevasTareas = [...tareas, texto];
+      setTareas([...tareas, texto]);
+
+      localStorage.setItem("tareas", JSON.stringify(nuevasTareas));
+      setTexto("");
+    }
+  };
+
+  const EliminarTarea = (index) => {
+    const nuevasTareas = tareas.filter((_, i) => i !== index);
+    setTareas(nuevasTareas);
+    localStorage.setItem("tareas", JSON.stringify(nuevasTareas));
+  };
+
+  return (
+    <div className="container">
+      <div className="todo-wrapper">
+        <h1>To Do List</h1>
+        <div className="input-group">
+          <input
+            type="text"
+            className="task-input"
+            onChange={ActualizarTexto}
+            value={texto}
+            placeholder="Añadir tarea"
+          />
+          <button className="add-btn" onClick={AgregarTarea}>
+            <FaPlus />
+          </button>
+        </div>
+        <div className="task-list">
+          {tareas.length === 0 ? (
+            <p className="empty-text">No hay tareas pendientes</p>
+          ) : (
+            tareas.map((tarea, index) => (
+              <div key={index} className="task-item">
+                <span>{tarea}</span>
+                <button
+                  className="delete-btn"
+                  onClick={() => EliminarTarea(index)}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Task.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Task = (props) => {
+  return (
+    <div className="taretita">
+      <div className="NameText">
+        <input type="checkbox" />
+        <p>{props.tarea}</p>
+      </div>
+      <div id="imagen" onClick={() => props.Eliminar(props.index)}></div>
+    </div>
+  );
+};
+
+export default Task;
